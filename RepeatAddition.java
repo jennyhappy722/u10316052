7e0ad4b48a601d05aeac6ab5528959cd37a2cd35
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-03-03 19:08:15 +0800

    add method getnumber2() getnumber1()
    add getnumber2()
    getnumber1()

--- RepeatAddition.java
+++ RepeatAddition.java
@@ -5,15 +5,22 @@ public class RepeatAddition{
     int addt;
     
   RepeatAddition(){
-    Scanner input= new Scanner(System.in);
-    addt=input.nextInt();
     }
     
   public int getaddts(){
+    Scanner input= new Scanner(System.in);
+    addt=input.nextInt();
     return addt;
   }
   
   public int getaddt(){
     return number1*number2;
   }
+    public int getnumber1(){
+		return number1;
+	}
+
+	public int getnumber2(){
+		return number2;
+	}
 }
