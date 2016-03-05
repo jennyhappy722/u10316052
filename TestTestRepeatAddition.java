1ed73e94826b108616c578185937183cbade5ad4
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-03-05 14:06:01 +0800

    Update TestTestRepeatAddition.java

--- TestTestRepeatAddition.java
+++ TestTestRepeatAddition.java
@@ -1,8 +1,10 @@
+//u10316052
 public class TestTestRepeatAddition{
 
 	public static void main(String[] args){
-		
+		//new TestRepeatAddition
 		TestRepeatAddition testRepeatAddition=new TestRepeatAddition();
+		//call TestRepeatAddition method
 		testRepeatAddition.ask();
 		
 
