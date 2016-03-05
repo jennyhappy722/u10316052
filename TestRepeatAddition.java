e565dba1a7158506ecfb9802855238f8b71e557b
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-03-05 14:26:33 +0800

    add annotate

--- TestRepeatAddition.java
+++ TestRepeatAddition.java
@@ -1,25 +1,30 @@
+//u10316052
+//import arraylist
 import java.util.*;
 public class TestRepeatAddition extends RepeatAddition{
-	
+	//new var and arraylist(int)
 	ArrayList <Integer> stack=new ArrayList<>();
 	int right=-100;
 	int i=0;
+	//addarray method
 	public void addtoArray(){
 		stack.add(getaddts());
 		gestsss();
 	}
 
-
+	//ask wheather right
 	public void ask(){
 
 		System.out.println("What is"+getnumber1()+"*"+getnumber2()+"?");
+		//let user input
 		gestsss();
 		for(i=0;i<=right;i++){
-	
+			//wheather input =right
 			if(getaddt()==getaddts()){					
 					System.out.print("你答對了!!");
 					i=-1000;
 			}else if(stack.contains(getaddts())){
+				//wheather repeat
 				System.out.println("不可以輸入重複的");
 				addtoArray();
 			}else{
