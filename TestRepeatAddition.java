701805c91738d16207629a93a92425d7d5db5154
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-03-05 14:03:00 +0800

    modify addtoArray(),ask()

--- TestRepeatAddition.java
+++ TestRepeatAddition.java
@@ -6,18 +6,22 @@ public class TestRepeatAddition extends RepeatAddition{
 	int i=0;
 	public void addtoArray(){
 		stack.add(getaddts());
+		gestsss();
 	}
 
 
 	public void ask(){
 
 		System.out.println("What is"+getnumber1()+"*"+getnumber2()+"?");
-
+		gestsss();
 		for(i=0;i<=right;i++){
-			addtoArray();
-			if(stack.contains(getaddt())){
-				System.out.print("你答對了!!");
-				right=i;
+	
+			if(getaddt()==getaddts()){					
+					System.out.print("你答對了!!");
+					i=-1000;
+			}else if(stack.contains(getaddts())){
+				System.out.println("不可以輸入重複的");
+				addtoArray();
 			}else{
 				System.out.println("你答錯了再一次~~~");
 				System.out.println("What is"+getnumber1()+"*"+getnumber2()+"?");
