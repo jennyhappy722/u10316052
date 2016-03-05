81609a6db83355caa32052a055cf17341b7b8bd9
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-03-05 14:04:01 +0800

    cantest  TestRepeatAddition

---
+++ TestTestRepeatAddition.java
@@ -0,0 +1,10 @@
+public class TestTestRepeatAddition{
+
+	public static void main(String[] args){
+		
+		TestRepeatAddition testRepeatAddition=new TestRepeatAddition();
+		testRepeatAddition.ask();
+		
+
+	}
+}
